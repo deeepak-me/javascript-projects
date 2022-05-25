8db61c7e888907ca2f8fdc1168febc7fdd33817c
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-05-25 17:10:28 +0530

    feat:todo app completed

--- todo app/script.js
+++ todo app/script.js
@@ -1,23 +1,76 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const todosUl = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => {
+        addTodo(todo);
+    });
+
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const todoText = input.value;
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.Text;
+    }
+
 
     if (todoText) {
         const todoEl = document.createElement('li');
+
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
 
         todoEl.innerText = todoText;
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+
+            updateLs();
+
+        });
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLs();
         })
 
-        todos.appendChild(todoEl);
-    }
+        todosUl.appendChild(todoEl);
+        input.value = "";
 
-    input.value = "";
-})
+        updateLs();
+    }
+}
+
+
+
+
+
+function updateLs() {
+    const todosEl = document.querySelectorAll('li');
+
+    const todos = []
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            Text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+}
