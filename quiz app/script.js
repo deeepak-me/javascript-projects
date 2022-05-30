c61d5077434c8ebe0e3ed25d6492805f8cff014e
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-05-30 23:22:28 +0530

    feat: qstns added

---
+++ quiz app/script.js
@@ -0,0 +1,39 @@
+const quisData = [
+
+    {
+        question: 'how old is deepak?',
+        a: '10',
+        b: '17',
+        c: '23',
+        d: '222',
+        correct: 'c'
+    }, {
+        question: 'best programming language?',
+        a: 'python',
+        b: 'javascript',
+        c: 'ruby',
+        d: 'c++',
+        correct: 'b'
+    }, {
+        question: 'who is the president of US?',
+        a: 'florin pop',
+        b: 'trump',
+        c: 'biden',
+        d: 'obama',
+        correct: 'c'
+    }, {
+        question: 'HTML stands for?',
+        a: 'Hypertext markup langauge',
+        b: 'Hypertext markup league',
+        c: 'dfaafasf',
+        d: 'afdgfgs',
+        correct: 'a'
+    }, {
+        question: 'js launched in?',
+        a: '2019',
+        b: '2020',
+        c: '1995',
+        d: '1996',
+        correct: 'c'
+    }
+]
